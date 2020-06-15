0f928c308a90910295f7f9495ec9da263791dcf5
someshsingh22 <someshsingh9414@gmail.com>
2020-06-15 22:31:54 +0530

    Decorator

--- scraper.py
+++ scraper.py
@@ -1,23 +1,31 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
+import time 
 
+HOME = 'https://www.instagram.com'
+LOGIN = 'https://www.instagram.com/accounts/login/?source=auth_switcher'
+
+def sleeper(sleep=3):
+    def decorator(action):
+        def wrapper(*args, **kwargs):
+            time.sleep(sleep)
+            action(*args, **kwargs)
+            time.sleep(sleep)
+        return wrapper
+    return decorator
+
+@sleeper(sleep=1)
 def login(driver,username,password,sleep=3):
-    driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
-    time.sleep(sleep)
+    go(driver,LOGIN)
     driver.find_element_by_name("username").send_keys(username)
     driver.find_element_by_name("password").send_keys(password)
     driver.find_element_by_name("password").send_keys(u'\ue007')
-    go_home(driver)
+    go(driver,HOME)
     NEXT_BUTTON_XPATH = "//button[@class='aOOlW   HoLwm ']"
     button = driver.find_element_by_xpath(NEXT_BUTTON_XPATH)
     button.click()
-    time.sleep(sleep)
 
-def go_home(driver, sleep=3):
-    time.sleep(sleep)
-    driver.get('https://www.instagram.com')
-    time.sleep(sleep)
-
-    
+@sleeper
+def go(driver, link, sleep=3):
+    driver.get(link)
